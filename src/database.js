f94d4be0ac34b0c3db248c524f6b75fabf08fed7
Kevin Martin <kevmartin2001@gmail.com>
2023-03-20 17:02:08 -0300

    cambiada la ip a 0

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://127.0.0.1:27017/cafeteria';
+const url = 'mongodb://0.0.0.0:27017/cafeteria';
 
 mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true});
 
